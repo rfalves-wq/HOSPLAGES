55ac9826c74fce596f63995521b7792d9aeed80a
User <rogerioparacambi@gmail.com>
2026-01-14 17:33:56 -0300

    atualid

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,15 +1,22 @@
-# usuarios/forms.py
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 class LoginForm(AuthenticationForm):
+
     username = forms.CharField(
-        label='Usuário',
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite seu usuário'}),
-        error_messages={'required': 'O campo usuário é obrigatório.'}
+        label="Usuário",
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Digite seu usuário',
+            'autocomplete': 'username'
+        })
     )
+
     password = forms.CharField(
-        label='Senha',
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Digite sua senha'}),
-        error_messages={'required': 'O campo senha é obrigatório.'}
+        label="Senha",
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Digite sua senha',
+            'autocomplete': 'current-password'
+        })
     )
